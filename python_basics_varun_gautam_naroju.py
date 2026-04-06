0330aab38a16c384754a2ac7a7dc41942fcd4203
varun <vnaroju9@gmail.com>
2026-04-06 11:46:27 -0700

    update python file

---
+++ python_basics_varun_gautam_naroju.py
@@ -0,0 +1,28 @@
+##Task 1:##
+
+person_name=input('Enter your name:')
+person_age=input('Enter your age:')
+person_height=input('Enter your height in meters:')
+person_is_student=input('Are you a student Yes or No:')
+
+print(f" {person_name}  {person_age}  {person_height}  {person_is_student}")
+
+##Task 2:##
+person_name=input('Enter your name:')
+person_age=input('Enter your age:')
+person_height=input('Enter your height in meters:')
+person_is_student=input('Are you a student Yes or No:')
+
+print(f"Name: {person_name} | Age: {person_age} | Height: {person_height} | Student: {person_is_student}")
+
+##Task 3:##
+
+person_name=input('Enter your name:')
+person_age=int(input('Enter your age:'))
+person_height=input('Enter your height in meters:')
+person_is_student=input('Are you a student Yes or No:')
+
+print("Age in Months:" ,person_age * 12)
+print("Age in days:" ,person_age * 365)
+print("The remainder when age is divided by 7:" ,person_age % 7)
+print("Age raised to the power of 2:", person_age**2)
