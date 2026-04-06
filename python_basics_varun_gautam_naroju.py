90b4996b66b4661b826c8854d7ea78715fcd727e
varun <vnaroju9@gmail.com>
2026-04-06 11:00:25 -0700

    initial commit

---
+++ python_basics_varun_gautam_naroju.py
@@ -0,0 +1,15 @@
+
+person_name=input('Enter your name:')
+person_age=input('Enter your age:')
+person_height=input('Enter your height in meters:')
+person_is_student=input('Are you a student Yes or No:')
+
+print(f" {person_name}  {person_age}  {person_height}  {person_is_student}")
+
+###
+
+print(f"Name: {person_name}, Age: {person_age}, Height: {person_height}, Student: {person_is_student}")
+print(f"Name : {person_name} , Age : {person_age} , Height : {person_height} , Student : {person_is_student}")
+print(f" {person_name}  , {person_age} ,  {person_height} ,  {person_is_student}")
+
+###
